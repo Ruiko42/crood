bb1e3feb314a7da24de6d8e3a1b008fc45348c87
Ruiko42 <pukaee16@gmail.com>
2026-06-15 11:35:19 +0500

    Delete CRUD directory

--- CRUD/connect.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$connect = mysqli_connect('localhost', 'root', '', 'products');
-
-if (!$connect) {
-    die('Error connect to database: ' . mysqli_connect_error());
-}
--- CRUD/vendor/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../connect.php';
-
-$id = $_GET['id'];
-
-mysqli_query($connect, "DELETE FROM `products` WHERE `products`.`id` = '$id'");
-
-header('Location: ../index.php');
